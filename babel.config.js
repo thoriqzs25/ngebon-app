8c97f835d5eef853b746c55c5c19597fae631492
Thariq Zhafran Satyagraha <71017985+thoriqzs25@users.noreply.github.com>
2023-03-14 11:37:01 +0700

    init new prj

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,22 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./'],
+          alias: {
+            '@src': './src',
+            '@components': './src/components',
+            '@assets': './src/assets',
+            '@cTypes': './src/types', // 'c' for custom
+            '@screens': './src/screens',
+            '@utils': './src/utils',
+          },
+        },
+      ],
+    ],
   };
 };
